0349af242fafc63e20bfa5f7a644ccc05415034e
Vinicios Wentz <wentz.vinicios@gmail.com>
2022-06-04 20:34:25 -0300

    removed the nonsense function

--- init.el
+++ init.el
@@ -1,20 +1,3 @@
-(defun global-unset-all-super-key ()
-  "Will unset any single key in global keymap that has the super
-modifier."
-  (let ((km (current-global-map)))
-    (while km
-      (let ((maybe-event (and (listp (car km))
-                              (caar km))))
-        (if (and (eventp maybe-event) ; Also filters maybe-event when
-                                      ; nil because (car km) was not a list.
-                 (memq 'super (event-modifiers maybe-event)))
-            (global-unset-key (vector maybe-event))))
-      (setq km (cdr km)))))
-
-
-(when (string-equal system-type "darwin")
-  (global-unset-all-super-key))
-
 (setq mac-pass-command-to-system nil)
 
 (setq inhibit-startup-message t)
