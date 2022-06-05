dabd96683081e113d5ceb4087ada6eef2172adbf
Vinicios Wentz <wentz.vinicios@gmail.com>
2022-06-04 21:40:44 -0300

    ivy and doom-modeline

--- init.el
+++ init.el
@@ -7,11 +7,54 @@
 (tooltip-mode -1)    ; Disable tooltips
 (menu-bar-mode -1)   ; Disable the menu bar
 
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
 (require 'package)
 
-(setq package-archives nil)
+(setq package-archives '(("melpa" . "http://melpa.org/packages/")))
 
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.org/packages/") t)
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
 
-(global-set-key [f8] 'neotree-toggle)
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(doom-modeline ivy use-package spacemacs-theme neotree lsp-mode elixir-mode centaur-tabs)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(use-package ivy
+  :diminish
+  :bind (("C-s" . swiper)
+         :map ivy-minibuffer-map
+         ("TAB" . ivy-alt-done)	
+         ("C-l" . ivy-alt-done)
+         ("C-j" . ivy-next-line)
+         ("C-k" . ivy-previous-line)
+         :map ivy-switch-buffer-map
+         ("C-k" . ivy-previous-line)
+         ("C-l" . ivy-done)
+         ("C-d" . ivy-switch-buffer-kill)
+         :map ivy-reverse-i-search-map
+         ("C-k" . ivy-previous-line)
+         ("C-d" . ivy-reverse-i-search-kill))
+  :config
+  (ivy-mode 1))
+
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1))
